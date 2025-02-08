4b7e3c4c99de331c77131d5d66ccd27a869cab1a
Yoshio Nomura <ynomura@juniper.net>
2025-02-08 19:14:38 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    serverRuntimeConfig: {
+        port: process.env.PORT || 3000, // Azure では `process.env.PORT` を使用
+    },
     env: {
         // Reference a variable that was defined in the .env file and make it available at Build Time
         API_ENDPOINT: process.env.API_ENDPOINT,
